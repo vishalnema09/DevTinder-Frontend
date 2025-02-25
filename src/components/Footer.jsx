14f716a15d15246a93dc192facfca9c8ddda27d1
vishal Nema <vishalnema23@gmail.com>
2025-02-25 12:37:41 +0530

    Edit Profile feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer bg-base-300 text-neutral-content items-center p-4 fixed bottom-0">
+    <footer className="footer bg-base-300 text-neutral-content items-center p-4 fixed bottom-0 flex justify-between">
       <aside className="grid-flow-col items-center">
         <svg
           width="36"
