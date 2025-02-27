4318f6286b5a8a65852a74c7d92ed8d3b3b87139
vishal Nema <vishalnema23@gmail.com>
2025-02-27 23:34:35 +0530

    Send/Ignore the user card from the feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -23,6 +23,10 @@ const Feed = () => {
   useEffect(() => {
     getFeed();
   }, []);
+  if (!feed) return;
+
+  if (feed.length <= 0)
+    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
 
   return (
     feed && (
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
+import { BASE_URL } from "../utils/constants";
 
 const UserCard = ({ user }) => {
-  const { firstName, lastName, photoUrl, age, gender, about } = user;
+  const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
+  const dispatch = useDispatch();
+
+  const handleSendRequest = async (status, userID) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userID ,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFromFeed(userID));
+    } catch (err) {}
+  };
+
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
       <figure>
@@ -9,11 +26,21 @@ const UserCard = ({ user }) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
-        {age && gender &&<p>{age + ", " +gender}</p>}
+        {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary">Ignored</button>
-          <button className="btn btn-secondary">Interested</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
